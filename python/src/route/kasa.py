6ddb7021fe337b193080c50d4dd54fd9b4f8a736
Foo JH <foojihaw@hotmail.com>
2024-05-18 17:09:38 +0700

    NEW: power on/off response

--- python/src/route/kasa.py
+++ python/src/route/kasa.py
@@ -20,7 +20,8 @@ async def init():
   ))
 
 async def updateDeviceCache(device):
-  print (device.device_type)
+  print (f'[kasa] discovered device: {device.alias}')
+  DiscoveredDevices[device.alias] = device
 
 @router.get('/discover')
 async def getDevices():
@@ -29,6 +30,7 @@ async def getDevices():
     on_discovered=updateDeviceCache
   ))
   ret = []
+  print (f'Discovered devices: {len(devices)}')
   for DeviceIpKey in devices:
     DiscoveredDevices[devices[DeviceIpKey].alias] = devices[DeviceIpKey]
     ret.append(devices[DeviceIpKey].alias)
@@ -36,21 +38,50 @@ async def getDevices():
   return ret
 
 @router.post('/device')
-def setDevice(cmd : dict):
+async def setDevice(evt : dict):
   KEY_DEVICE_ALIAS = 'DeviceAlias'
 
-  print (cmd)
-  if not KEY_DEVICE_ALIAS in cmd:
+  print (evt)
+  if not KEY_DEVICE_ALIAS in evt:
     raise HTTPException (
       status_code=400,
       detail = 'Missing DeviceAlias'
-      )
+    )
 
-  DeviceAlias = cmd[KEY_DEVICE_ALIAS]
+  DeviceAlias = evt[KEY_DEVICE_ALIAS]
   if not DeviceAlias in DiscoveredDevices.keys():
     raise HTTPException (
       status_code=400,
       detail = f'Unknown device alias "{DeviceAlias}"'
+    )
+
+  TargetDevice = DiscoveredDevices[DeviceAlias]
+  # get the latest state
+  await asyncio.create_task(TargetDevice.update())
+
+  if 'cmd' in evt:
+    ret = None
+    if 'power' in evt['cmd']:
+      if evt['cmd']['power'] == 'on':
+        print ('[kasa] power on')
+        ret = await asyncio.create_task(DiscoveredDevices[DeviceAlias].turn_on()) 
+      elif evt['cmd']['power'] == 'toggle':
+        print (f'Device power state: {"on" if TargetDevice.is_on else "off"}')
+        if TargetDevice.is_on:
+          await asyncio.create_task(TargetDevice.turn_off())
+          await asyncio.create_task(TargetDevice.update())
+        else:
+          await asyncio.create_task(TargetDevice.turn_on())
+          await asyncio.create_task(TargetDevice.update())
+      else:
+        print ('[kasa] power off')
+        ret = await asyncio.create_task(DiscoveredDevices[DeviceAlias].turn_off())
+
+      return ret
+    else:
+      raise HTTPException (
+        status_code=400,
+        detail = f'Unknown command {evt["cmd"]}'
       )
 
   return DiscoveredDevices[DeviceAlias].device_type
